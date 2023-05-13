c9ed15c37ca62a57ff4bfe0f34de0fc094317887
htetkhine <htetgyi957@gmail.com>
2023-05-13 23:53:37 +0630

    first commit

---
+++ promise.js
@@ -0,0 +1,32 @@
+let request = (resource)=>{
+
+    return new Promise((resolve,reject)=>{
+
+        let xhr =new XMLHttpRequest;
+        xhr.addEventListener("readystatechange",function(){
+            if(xhr.readyState === 4 && xhr.status === 200){
+                let data = this.responseText;
+                let jsonData = JSON.parse(data);
+                resolve(jsonData);
+            }else if(xhr.status === 404){
+                reject('something was wrong');
+            }
+        })
+    
+        xhr.open('GET', resource);
+        xhr.send();
+    })    
+}
+
+request('hk.json')
+.then((data)=>{
+    console.log(data);
+    return request('bio.json');
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
