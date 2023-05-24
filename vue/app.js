4edb9b4e8979e0b6bc436a31cf087ebd7452a38a
htetkhine <htetgyi957@gmail.com>
2023-05-25 00:08:55 +0630

    for loop and v-bind and :class

--- vue/app.js
+++ vue/app.js
@@ -1,17 +1,44 @@
 const app = Vue.createApp({    
     data() {
-        return {
-          Title: 'this is vue here',
-          Name: 'Htet Khine',
-          Age:20,
-          changeState: true
+        return{
+          Books:[
+            {
+              Title: 'Beginner to ninja Vuejs',
+              Name: 'Htet Khine',
+              Age:20,
+              isFav: true
+            },
+            {
+              Title: 'How to breath in water?',
+              Name: 'Mr:Htet',
+              Age:20,
+              isFav: false
+            },
+            {
+              Title: 'Say! loundly Hello World',
+              Name: 'Htet Khine',
+              Age:20,
+              isFav: true
+            },
+          ],
+          changeState : true                   
         }
       },
     methods: {
       handle(){
         this.Age++
+      },
+      favHandler(Book){
+        Book.isFav = !Book.isFav
       }
     },
+    computed:{
+      filteredBook(){
+        return this.Books.filter(Book=>{
+          return Book.isFav;
+        })
+      }
+    }
 })
 
 app.mount('#app');
