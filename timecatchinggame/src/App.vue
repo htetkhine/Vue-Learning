17036b4d39d82d6f59b8e7b02855a2c8038562e4
htetkhine <htetgyi957@gmail.com>
2023-06-06 23:59:12 +0630

    mounted and settimeout

--- timecatchinggame/src/App.vue
+++ timecatchinggame/src/App.vue
@@ -1,6 +1,9 @@
 <template>   
   <h1>Time Catching Game</h1>
-  <Home></Home>
+  <button @click="clickWorking" :disabled="isPlaying" class="my-3">Click Me</button>
+  <div v-if="isPlaying">
+    <Home :timeRanges="timeRanges"></Home>
+  </div>  
 </template>
 
 <script>
@@ -9,7 +12,19 @@ export default {
   name: 'App',
   components: {  
     Home
-  }
+  },
+  data() {
+    return {
+      isPlaying: false,
+      timeRanges: null
+    }
+  },
+  methods: {
+    clickWorking(){
+        this.isPlaying = true;
+        this.timeRanges = 2000+Math.random()*5000;
+    }
+  },  
 }
 </script>
 
--- timecatchinggame/src/components/Home.vue
+++ timecatchinggame/src/components/Home.vue
@@ -1,13 +1,47 @@
 <template>
-    <div>
+    <div>        
+        <div v-if="isPlaying">
+            <div class="box">
+                {{ timeRanges }}       
+            </div>
+        </div>
         
     </div>
 </template>
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    props:['timeRanges'],
+    data() {
+        return {
+            isPlaying: false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.isPlaying = true;
+        }, this.timeRanges);        
+    },
+    created(){
+        console.log('it is created');
+    },
+    beforeCreate(){
+        console.log('it is beforecreated');
+    },
+    updated(){
+        console.log('it is updated');
+    },
+    beforeUpdate(){
+        console.log('it is beforeupdate');
+    }
 }
 </script>
 <style scoped>
-    
+    .box{
+        width: 300px;
+        height: 300px;
+        background-color: yellow;
+        display: block;
+        margin: 0 auto;
+    }
 </style>
