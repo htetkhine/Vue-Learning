8887839c1aad7a607a340eea942afd1e30c484c5
htetkhine <htetgyi957@gmail.com>
2023-06-25 21:59:50 +0630

    create project

--- project_planner/src/router/index.js
+++ project_planner/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CreateProject from '../views/CreateProjects.vue'
 
 const routes = [
   {
@@ -14,6 +15,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/createProject',
+    name: 'createProject',
+    component: CreateProject,
+    props: true
   }
 ]
 
---
+++ project_planner/src/views/CreateProjects.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <van-form @submit="onSubmit">
+            <van-cell-group inset>
+                <van-field v-model="text" label="Title" />                
+                <van-field v-model="number" type="number" label="ID" />  
+                <van-field
+                    v-model="message"
+                    rows="2"
+                    autosize
+                    label="Message"
+                    type="textarea"
+                    maxlength="50"
+                    placeholder="Message"
+                    show-word-limit
+                />            
+            </van-cell-group>
+            <div style="margin: 16px;">
+                <van-button round block type="primary" native-type="submit">
+                Submit
+                </van-button>
+            </div>
+        </van-form>
+    </div>    
+
+
+</template>
+
+<script setup>
+    import { Form, Field, CellGroup } from 'vant';
+    import {Button} from 'vant';
+    import { ref } from 'vue'; 
+    const emits = defineEmits(['create']);  
+    function onSubmit(){
+        emits('create',[{
+            id:number,
+            title:text,
+            description:message
+        }])
+    }
+   
+</script>
+
+<style lang="scss" scoped>
+    form{
+        width: 500px;
+        max-width: 300px;
+        display: block;
+        margin: 0 auto;
+        
+    }
+</style>
