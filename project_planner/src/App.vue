ac22ebb5d3dd6cd289adc036cfe3a697404f5e73
htetkhine <htetgyi957@gmail.com>
2023-07-29 00:54:46 +0630

    footer create

--- project_planner/src/App.vue
+++ project_planner/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link :to="{name:'about', params: { receivedData: 'helo' }}">About</router-link> |
-    <router-link :to="{name :'createProject'}">Create</router-link>    
-  </nav>
+  <header> 
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link :to="{name:'about', params: { receivedData: 'helo' }}">About</router-link> |
+      <router-link :to="{name :'createProject'}">Create</router-link>    
+    </nav>
+  </header>
+ 
   <router-view/>
+
+  <footer>
+      <h6>Develop By Htet Khine</h6>
+  </footer>
 </template>
 <script setup>
 import { ref } from "vue";
@@ -20,7 +27,8 @@ import { defineProps } from "vue";
   color: #2c3e50;
 }
 
-nav {
+nav,
+footer {
   padding: 30px;
   background-color:#2c3e50;
   a {
@@ -31,9 +39,17 @@ nav {
       color: #42b983;
     }
   }
+  h6{
+    color: #fff;
+  }
   .van-search{
     display: inline-block;
     background: transparent;
   }
+}
+footer{
+  position:fixed;
+  bottom:0;
+  width:100%;
 }
 </style>
